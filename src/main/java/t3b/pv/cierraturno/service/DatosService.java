b7237c48c53edf4856b0468412f205e261e42830
EricKingtg <ericktampicoxxi@hotmail.com>
2020-07-31 10:58:16 -0500

    Version que agrega la parte para que funcione con EnvioAutomatico

--- src/main/java/t3b/pv/cierraturno/service/DatosService.java
+++ src/main/java/t3b/pv/cierraturno/service/DatosService.java
@@ -7,12 +7,25 @@ import t3b.pv.cierraturno.dto.TurnoDto;
 public interface DatosService {
 
 	public abstract void init();
+
 	public abstract void proceso(int idTurno, int bandera);
-	//////////////////////////
+
 	abstract List<TurnoDto> consultaTurnosNoEnviadosExt();
+
 	abstract void consultaTurnosNoEnviados();
+
 	abstract void iniciaTurnoBOT();
+
 	abstract void borraInfoTurno(int idTurno);
+
 	abstract void procesoCierraTurnoBOT(int idTurno);
-	
+
+	public String getClave();
+
+	public void setClave(String clave);
+
+	public String getCaja();
+
+	public void setCaja(String caja);
+
 }
